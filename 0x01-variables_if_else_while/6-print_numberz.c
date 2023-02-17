4baa7f8a1ce53896c7a61a4ab4bdb4aea3671b17
Doaa-w910rgb <doaa.samysaleh@gmail.com>
2023-02-17 10:15:12 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d = 100;
+
+	while (d < 110)
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
+}
