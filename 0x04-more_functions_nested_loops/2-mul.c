6dd877d0eaf210dd89df79ff9c37933ec419c5e7
Doaa-w910rgb <doaa.samysaleh@gmail.com>
2023-02-22 23:56:48 -0800

    0-isupper.c,1-isdigit.c,2-mul.c,3-print_numbers.c,4-print_most_numbers.c,5-more_numbers.c,README.md,main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - returns of multiplication between two integers
+ * @a: frist multiplier
+ * @b: second multiplier
+ *
+ *return: multiplication result
+ */
+int mul(int , int b)
+{
+	int result;
+
+	result=a*b;
+	return result;
+}
